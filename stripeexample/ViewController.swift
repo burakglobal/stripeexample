dcae266ce23117fbb1c9d81681d9465884fd6d30
Megha Gulati <meghagulati30@gmail.com>
2015-01-03 22:46:26 -0800

    Initial Commit

---
+++ stripeexample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stripeexample
+//
+//  Created by MEGHA GULATI on 1/3/15.
+//  Copyright (c) 2015 MEGHA GULATI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
